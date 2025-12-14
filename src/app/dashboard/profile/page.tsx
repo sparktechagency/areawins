74a8b58d361afd9d8fbe7b10d01b7e51b0fc0158
rakibislam2233 <rakib2020.tkg@gmail.com>
2025-12-14 14:19:50 +0600

    Update profile page and add edit-profile section

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -8,6 +8,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useAuth } from "@/hooks/useAuth";
 import { User } from "lucide-react";
+import Link from "next/link";
 
 export default function ProfilePage() {
    const { user } = useAuth();
